235f30a7969122f7ffd784a3276e9a9e96964942
JAYPEE <eyibrajohnpaul@gmail.com>
2025-01-30 12:28:35 +0100

    update

--- src/modules/landing-page/components/HowItWorks.tsx
+++ src/modules/landing-page/components/HowItWorks.tsx
@@ -1,7 +1,38 @@
+"use client";
 import BlurIn from "@/components/animations/blur-in";
 import { HowItWorksItem } from "./HowItWorksItem";
+import { useEffect, useRef, useState } from "react";
 
 export function HowItWorks() {
+  function useIsVisible(ref: any) {
+    const [isIntersecting, setIntersecting] = useState(false);
+
+    useEffect(() => {
+      const observer = new IntersectionObserver(([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      });
+
+      observer.observe(ref.current);
+      return () => {
+        observer.disconnect();
+      };
+    }, [ref]);
+
+    return isIntersecting;
+  }
+
+  const ref1 = useRef<HTMLDivElement>(null);
+  const isVisible1 = useIsVisible(ref1);
+
+  const ref2 = useRef<HTMLDivElement>(null);
+  const isVisible2 = useIsVisible(ref2);
+
+  const ref3 = useRef<HTMLDivElement>(null);
+  const isVisible3 = useIsVisible(ref3);
+
+  const ref4 = useRef<HTMLDivElement>(null);
+  const isVisible4 = useIsVisible(ref3);
+
   return (
     <section className="grid items-center gap-5 px-4 py-20 md:grid-cols-2 md:px-12">
       <div>
@@ -16,20 +47,46 @@ export function HowItWorks() {
         </p>
       </div>
 
-      <div>
+      <div
+        ref={ref1}
+        className={`transition-opacity duration-700 ease-in ${
+          isVisible1 ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <HowItWorksItem
           subTitle="Choose a phone number, voice, language, give it a name to create a custom AI agent that reflects your brand."
           title="Set up your AI agent"
           isActive
         />
+      </div>
+      <div
+        ref={ref2}
+        className={`transition-opacity duration-700 ease-in ${
+          isVisible2 ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <HowItWorksItem
           subTitle="Add company information and scripts to empower your AI agent with the tools to handle customer inquiries like a pro."
           title="Upload to knowledge base"
         />
+      </div>
+      <div
+        ref={ref3}
+        className={`transition-opacity duration-700 ease-in ${
+          isVisible3 ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <HowItWorksItem
           subTitle="Make your assigned phone number available to your customers and watch as incoming calls are resolved instantly with speed and efficiency."
           title="Go live"
         />
+      </div>
+      <div
+        ref={ref4}
+        className={`transition-opacity duration-700 ease-in ${
+          isVisible4 ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <HowItWorksItem
           subTitle="Handle over 1 million concurrent calls with our Kubernetes-based infrastructure designed for scalability and high availability."
           title="Scale with ease"
