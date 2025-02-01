c7ea94706e8088698fdee556460e9bc9da7f367a
sambalicious <stemitope370@gmail.com>
2025-02-01 10:07:43 +0100

    refactor: update ESLint and Prettier configurations, add format script, and clean up code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
 
 // Exclude public routes
-// const publicRoutes = [
-//   "/login",
-//   "/register",
-//   "/forgot-password",
-//   "/verify-email",
-//   "/reset-password",
-//   "/pricing",
-//   "/contact-us",
-//   "/about-us",
-//   "/terms",
-// ];
+const publicRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/verify-email",
+  "/reset-password",
+  "/pricing",
+  "/contact-us",
+  "/about-us",
+  "/terms",
+];
 
-export async function middleware() {
-  // const { pathname } = req.nextUrl;
-  // if (publicRoutes.includes(pathname)) {
-  //   return NextResponse.next();
-  // }
-  // const authCookie = req.cookies.get("adora-auth-cookie");
-  // if (!authCookie?.value) {
-  //   // Redirect unauthenticated users to login
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-  // return NextResponse.next();
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+  const authCookie = req.cookies.get("adora-auth-cookie");
+  if (!authCookie?.value) {
+    // Redirect unauthenticated users to login
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  return NextResponse.next();
 }
 
 export const config = {
