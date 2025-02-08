c14074953b0d2d371e173750196092280a88c83a
sambalicious <stemitope370@gmail.com>
2025-02-08 22:04:17 +0100

    fix: add null when business has error

--- src/modules/dashboard/components/BusinessSetUpModal.tsx
+++ src/modules/dashboard/components/BusinessSetUpModal.tsx
@@ -21,13 +21,13 @@ export function BusinessSetupModal(props: Readonly<Disclosure>) {
     queryKey: ["getBusiness"],
   });
 
-  const business = data?.data;
+  const business = data?.data ?? null;
 
   useEffect(() => {
     if (!isPending && !business?.isProfileCompleted) {
       onOpen();
     }
-  }, [business, onOpen]);
+  }, [business, onOpen, isPending]);
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
