7fb62cf869a59f0a182cc8d838eb4b2957da22e8
sambalicious <stemitope370@gmail.com>
2025-04-06 22:58:45 +0100

    ci: comment out migration steps in CI workflow and adjust query key factory

--- src/lib/queryKeyFactory.ts
+++ src/lib/queryKeyFactory.ts
@@ -1,7 +1,7 @@
 import { createServerActionsKeyFactory } from "zsa-react-query";
 
 export const QueryKeyFactory = createServerActionsKeyFactory({
-  getBusinessProfile: (id: string) => ["getBusinessProfile", id] as string[],
+  getBusinessProfile: () => ["getBusinessProfile"] as string[],
   getUser: (id: string) => ["getUser", id] as string[],
   getKnowledgeBase: (id: string) => ["getKnowledgeBase", id] as string[],
 });
