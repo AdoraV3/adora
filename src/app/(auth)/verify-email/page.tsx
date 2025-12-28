afae90cfb3073e9362796bd58fe4afa0675c24f0
Okwoli <okwolig60@gmail.com>
2025-12-28 15:30:14 +0100

    update

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-export const dynamic = "force-dynamic"
-
 'use client'
+
+export const dynamic = "force-dynamic"
 
 import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
